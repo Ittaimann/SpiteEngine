17adb17f017ee8455def9c3f4431659228f66f89
Ittaimann <ittaiam@gmail.com>
2020-08-23 18:48:06 -0700

    set up a new spite engine project, with cmake again.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    std::cout<<"cmake is a nightmare"<<std::endl;
+    return 1;
+}
